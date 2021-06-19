1137ac5569bff8a41b5360a0bf222d1bd6f98842
jaredwuu <jiachaowu@hotmail.com>
2021-06-19 00:46:30 -0400

    up

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useState ,useEffect} from 'react';
 import Editor from './Editor'
 function App() {
   const [html, setHtml] = useState('')
   const [css, setCss] = useState('')
   const [js, setJs] = useState('')
+  const [srcDoc, setSrcDoc] = useState('')
+  useEffect(() => {
+    const timeout = setTimeout(() =>{
+      setSrcDoc(`
+        <html> 
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    },500)
+    return () =>clearTimeout(timeout)
+  }, [html,css,js])
+
   return (
     <>
-      <div className="flex justify-between px-10 py-3 bg-gray-900">
+      <div className="flex px-10 py-3 bg-gray-900">
         <Editor
           language="xml"
           displayName='HTML'
@@ -28,8 +42,15 @@ function App() {
 
 
       </div>
-      <div className = 'bg-yellow-100 h-52'>
-        <h1> hollo </h1>
+      <div className = 'bg-yellow-100 h-screen'>
+        <iframe
+          srcDoc = {srcDoc}
+          title='output'
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width = '100%'
+          height = '100%'  
+        />
       </div>
     </>
   );
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCompressAlt,faExpandAlt} from '@fortawesome/free-solid-svg-icons'
 
 const Editor = (props) => {
     const {
@@ -14,21 +16,29 @@ const Editor = (props) => {
         onChange
     } = props
 
-    function handleChange(editor, data, value) {
+    const [open, setOpen] =useState(true);
+
+
+    function handleChange(editor, data, value){
         onChange(value)
     }
     return (
-        <div className='text-white w-1/3 px-2 rounded-md'>
-            <div className='flex px-2 justify-between bg-gray-700 h-8 items-center'>
+        <div className={`text-white w-1/3 px-2 rounded-md `}>
+            <div className={`flex px-2 justify-between ${open ? '':'flex-none items-left w-1/3'} rounded-t-md bg-gray-700 h-8 items-center`}>
                 <div>
                     {displayName}
                 </div>
-                <button className='bg-gray-600 rounded-sm'> O/C</button>
+                <button 
+                className='bg-gray-600 rounded-sm'
+                onClick={()=>setOpen(prevOpen=>!prevOpen)}
+                >
+                    <FontAwesomeIcon icon={open? faCompressAlt:faExpandAlt} />
+                </button>
             </div>
             <ControlledEditor
                 onBeforeChange={handleChange}
                 value={value}
-                className='code-mirror-wrapper'
+                className={`${open ? '':'flex-none items-left w-1/3'} rounded-t-md `}
                 options={{
                     lineWrapping: true,
                     lint: true,
